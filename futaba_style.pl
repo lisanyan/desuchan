ade506b044fa2d5d79ebbe05431f3ad30e591171
k_anon <k_anon@4871d5c7-3bdd-4872-8bc4-3d792a1ad158>
2008-03-25 21:36:35 +0000

    Global Moderator display fix in Staff Log
    git-svn-id: https://suigintou.weedy.ca/svn/desuchan/trunk@32 4871d5c7-3bdd-4872-8bc4-3d792a1ad158

--- futaba_style.pl
+++ futaba_style.pl
@@ -1001,8 +1001,8 @@ use constant STAFF_ACTIVITY_UNFILTERED => compile_template(MANAGER_HEAD_INCLUDE.
 		<tr class="row<var $rowtype>">
 			<td><if $disabled><span style="color:#555555"></if><strong><var $username></strong><if $disabled></span></if></td>
 			<td><if $account eq 'admin'>Administrator</if>
-			<if $account eq 'mod'>Moderator</if></td>
-			<if $type eq 'globmod'>Global Moderator</if>
+			<if $account eq 'mod'>Moderator</if>
+			<if $account eq 'globmod'>Global Moderator</if></td>
 			<td><var get_action_name($action)></td>
 			<td><var get_action_name($action,2)>: <var $info></td>
 			<td><var $date></td>
